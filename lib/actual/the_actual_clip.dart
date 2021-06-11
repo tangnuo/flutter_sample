18567e0f2cb32b513ad0ee1452c67d7ca307ac3e
caoweijian <caoweijian@kedacom.com>
2021-06-11 16:51:53 +0800

    图片加载

--- lib/actual/the_actual_clip.dart
+++ lib/actual/the_actual_clip.dart
@@ -13,17 +13,27 @@ class ClipTestRoute extends StatelessWidget {
 
   Center buildBody() {
     // 头像
+    /// 本地图片加载的三种方式：
     Widget avatar = Image.asset("images/yuner1.jpg", width: 160.0);
+    Widget avatar2 = Image(
+      image: AssetImage("images/yuner1.jpg"),
+      width: 160.0,
+    ); //AssetImage没有scale属性
+
+    Widget avatar3 = Image(
+      image: ExactAssetImage("images/yuner1.jpg", scale: 1),
+      width: 160.0,
+    ); //scale属性默认值为1
 
     return Center(
       child: Column(
         children: <Widget>[
           avatar, //不剪裁
-          ClipOval(child: avatar), //剪裁为圆形
+          ClipOval(child: avatar2), //剪裁为圆形
           ClipRRect(
             //剪裁为圆角矩形
             borderRadius: BorderRadius.circular(5.0),
-            child: avatar,
+            child: avatar3,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
